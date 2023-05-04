e7e5a8f3e348b5cd6880601056f775b4e3573343
Tim McFadden <mtik00@users.noreply.github.com>
2023-05-04 15:58:17 -0600

    feat: start of edit modal

--- app/app.py
+++ app/app.py
@@ -2,9 +2,10 @@
 # -*- coding: utf-8 -*-
 
 from textual.app import App, Binding, ComposeResult
-from textual.containers import Container, VerticalScroll
+from textual.containers import Container, Grid, VerticalScroll
 from textual.reactive import reactive
-from textual.widgets import Footer, Header, ListItem, ListView, Static
+from textual.screen import ModalScreen
+from textual.widgets import Button, Footer, Header, Input, ListItem, ListView, Static
 
 from .db import Address, get_labels_for_address
 
@@ -19,7 +20,7 @@ class AddressListItem(ListItem):
 
     def watch_highlighted(self, value: bool):
         if value:
-            app.new_address(self.address)
+            self.app.new_address(self.address)
 
         super().watch_highlighted(value)
 
@@ -44,11 +45,38 @@ class AddressInfoWidget(Static):
         return ""
 
 
+class EditScreen(ModalScreen):
+    """Screen with a dialog to quit."""
+
+    def __init__(self, address, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.address = address
+
+    def compose(self) -> ComposeResult:
+        yield Grid(
+            Input(value=self.address.name),
+            Button("Ok", variant="primary", id="ok"),
+            Button("Cancel", variant="primary", id="cancel"),
+            id="dialog",
+        )
+
+    def on_button_pressed(self, event: Button.Pressed) -> None:
+        if event.button.id == "cancel":
+            self.app.pop_screen()
+        else:
+            print("TODO: Modify the address")
+            self.address.name = self.query_one(Input).value
+            self.address.save()
+            self.app.pop_screen()
+            # self.app.refresh(layout=True)
+
+
 class AddressBookApp(App):
     CSS_PATH = "css/app.css"
 
     BINDINGS = [
-        Binding(key="q", action="quit", description="Quit the app"),
+        Binding(key="q", action="quit", description="Quit"),
+        Binding(key="e", action="edit", description="Edit"),
     ]
 
     def compose(self) -> ComposeResult:
@@ -67,6 +95,13 @@ class AddressBookApp(App):
     def new_address(self, address: Address) -> None:
         widg = self.query_one(AddressInfoWidget)
         widg.address = address
+
+    def action_edit(self):
+        address = self.query_one(AddressInfoWidget).address
+        self.push_screen(EditScreen(address))
+        # TODO: Figure out how to redraw the ListItem
+        # self.query_one(AddressListItem, address.name).refresh()
+        # self.compose()
 
 
 app = AddressBookApp()
