7f9f12cf6476de408dca4ef2317f1adb6d14680f
Tim McFadden <mtik00@users.noreply.github.com>
2023-05-06 09:05:47 -0600

    refactor: move edit modal

--- address_book/app.py
+++ address_book/app.py
@@ -3,12 +3,12 @@
 import logging
 
 from textual.app import App, Binding, ComposeResult
-from textual.containers import Container, Horizontal, Vertical, VerticalScroll
+from textual.containers import Container, VerticalScroll
 from textual.reactive import reactive
-from textual.screen import ModalScreen
-from textual.widgets import Button, Footer, Header, Input, ListItem, ListView, Static
+from textual.widgets import Footer, Header, ListItem, ListView, Static
 
 from .db import Address, Label, add_label_to_address, get_labels_for_address
+from .modals.edit_address import EditAddressScreen
 
 log = logging.getLogger(__name__)
 
@@ -53,49 +53,6 @@ class AddressInfoWidget(Static):
         return ""
 
 
-class EditScreen(ModalScreen):
-    """Screen with a dialog to quit."""
-
-    def __init__(self, address, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.address = address
-
-    def compose(self) -> ComposeResult:
-        with Vertical():
-            yield Input(value=self.address.name, id="addr-name")
-            yield Input(value=self.address.street, id="addr-street")
-            yield Input(value=self.address.city, id="addr-city")
-            yield Input(value=self.address.state, id="addr-state")
-            yield Input(value=self.address.zipcode, id="addr-zipcode")
-            with Horizontal():
-                yield Button("Ok", variant="primary", id="ok")
-                yield Button("Cancel", variant="primary", id="cancel")
-
-    def on_button_pressed(self, event: Button.Pressed) -> None:
-        if event.button.id == "cancel":
-            self.app.pop_screen()
-        else:
-            self.address.name = self.query_one("#addr-name", Input).value
-            self.address.street = self.query_one("#addr-street", Input).value
-            self.address.city = self.query_one("#addr-city", Input).value
-            self.address.state = self.query_one("#addr-state", Input).value
-            self.address.zipcode = self.query_one("#addr-zipcode", Input).value
-            self.address.save()
-            self.app.pop_screen()
-
-            # Update the widgets we just modified
-            wdg: ListView = self.app.query_one("#address-listview", ListView)
-
-            item: ListItem | None = wdg.highlighted_child
-            if item:
-                item.refresh(layout=True)
-
-            self.app.query_one("#address-info").refresh()
-
-    def key_escape(self):
-        self.app.pop_screen()
-
-
 class AddressBookApp(App):
     CSS_PATH = "css/app.css"
 
@@ -128,7 +85,7 @@ class AddressBookApp(App):
 
     def action_edit(self):
         address = self.query_one(AddressInfoWidget).address
-        self.push_screen(EditScreen(address))
+        self.push_screen(EditAddressScreen(address))
 
     def action_select(self):
         # Update the widgets we just modified
---
+++ address_book/modals/edit_address.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from textual.app import ComposeResult
+from textual.containers import Horizontal, Vertical
+from textual.screen import ModalScreen
+from textual.widgets import Button, Input, ListItem, ListView
+
+
+class EditAddressScreen(ModalScreen):
+    """Screen with a dialog to quit."""
+
+    def __init__(self, address, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.address = address
+
+    def compose(self) -> ComposeResult:
+        with Vertical():
+            yield Input(value=self.address.name, id="addr-name")
+            yield Input(value=self.address.street, id="addr-street")
+            yield Input(value=self.address.city, id="addr-city")
+            yield Input(value=self.address.state, id="addr-state")
+            yield Input(value=self.address.zipcode, id="addr-zipcode")
+            with Horizontal():
+                yield Button("Ok", variant="primary", id="ok")
+                yield Button("Cancel", variant="primary", id="cancel")
+
+    def on_button_pressed(self, event: Button.Pressed) -> None:
+        if event.button.id == "cancel":
+            self.app.pop_screen()
+        else:
+            self.address.name = self.query_one("#addr-name", Input).value
+            self.address.street = self.query_one("#addr-street", Input).value
+            self.address.city = self.query_one("#addr-city", Input).value
+            self.address.state = self.query_one("#addr-state", Input).value
+            self.address.zipcode = self.query_one("#addr-zipcode", Input).value
+            self.address.save()
+            self.app.pop_screen()
+
+            # Update the widgets we just modified
+            wdg: ListView = self.app.query_one("#address-listview", ListView)
+
+            item: ListItem | None = wdg.highlighted_child
+            if item:
+                item.refresh(layout=True)
+
+            self.app.query_one("#address-info").refresh()
+
+    def key_escape(self):
+        self.app.pop_screen()
